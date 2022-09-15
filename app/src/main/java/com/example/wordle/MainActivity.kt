e7a245722334e065040be3f929546b41ec40d3ec
kiara-bermudez <kiarabermudez.10@gmail.com>
2022-09-15 02:38:30 -0500

    Implemented logic for guesses 2 and 3.

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -24,9 +24,14 @@ class MainActivity : AppCompatActivity() {
         val guessButton = findViewById<Button>(R.id.guessBtn)
         val answer = findViewById<TextView>(R.id.wordToGuess)
         val editText = findViewById<EditText>(R.id.editText)
-        val userInput = editText.text
+
+        // Set answer to random word we got earlier
+        answer.text = wordToGuess
 
         guessButton.setOnClickListener {
+            // Get user input
+            val userInput = editText.text.toString().uppercase()
+
             // Display a toast if user input is not a 4 letter word
             if (userInput.length != 4) {
                 Toast.makeText(it.context, "Your guess should contain 4 letters!", Toast.LENGTH_SHORT).show()
@@ -46,6 +51,44 @@ class MainActivity : AppCompatActivity() {
                     // Make guess #1 and guess #1 check text views visible
                     g1txt.visibility = View.VISIBLE
                     g1ctxt.visibility = View.VISIBLE
+                }
+                // Guess 2
+                else if (guessNum == 2) {
+                    // Get reference to relevant guess 2 text views
+                    val g2 = findViewById<TextView>(R.id.g2)
+                    val g2c = findViewById<TextView>(R.id.g2c)
+                    val g2txt = findViewById<TextView>(R.id.g2txt)
+                    val g2ctxt = findViewById<TextView>(R.id.g2ctxt)
+
+                    // Update guess #2 and guess #2 check text views
+                    g2txt.text = userInput.toString()
+                    g2ctxt.text = checkGuess(userInput.toString())
+
+                    // Make guess #2 and guess #2 check text views visible
+                    g2txt.visibility = View.VISIBLE
+                    g2ctxt.visibility = View.VISIBLE
+                }
+                // Guess 3 - Game ends
+                else {
+                    // Get reference to relevant guess 3 text views
+                    val g3 = findViewById<TextView>(R.id.g3)
+                    val g3c = findViewById<TextView>(R.id.g3c)
+                    val g3txt = findViewById<TextView>(R.id.g3txt)
+                    val g3ctxt = findViewById<TextView>(R.id.g3ctxt)
+
+                    // Update guess #3 and guess #3 check text views
+                    g3txt.text = userInput
+                    g3ctxt.text = checkGuess(userInput)
+
+                    // Make guess #3 and guess #3 check text views visible
+                    g3txt.visibility = View.VISIBLE
+                    g3ctxt.visibility = View.VISIBLE
+
+                    // Game is over so make answer visible
+                    answer.visibility = View.VISIBLE
+
+                    // Reset guessNum
+                    guessNum = 0
                 }
 
                 // Increment guessNum
